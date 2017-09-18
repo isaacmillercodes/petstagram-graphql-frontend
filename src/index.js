2d36ec8359c183558192960c99f01dfecfafc000
Isaac McGaha Miller <isaacmcgahamiller@gmail.com>
2017-09-18 06:25:04 -0600

    added apollo to index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloProvider, createNetworkInterface, ApolloClient } from 'react-apollo'
 import './styles/index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const networkInterface = createNetworkInterface({
+  uri: 'http://localhost:5000/graphql'
+})
+
+const client = new ApolloClient({
+  networkInterface
+})
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+  , document.getElementById('root')
+);
 registerServiceWorker();
