63492575e11b3ef99ea47a7f28037d194147edef
Isaac McGaha Miller <isaacmcgahamiller@gmail.com>
2017-09-18 20:04:34 -0600

    add pet now working

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom'
 import SplashPage from './SplashPage';
 import AuthForm from './AuthForm';
 import UserProfile from './UserProfile';
+import AddPet from './AddPet';
 import PetProfile from './PetProfile';
 
 class App extends Component {
@@ -12,6 +13,7 @@ class App extends Component {
         <Route exact path='/' component={SplashPage}/>
         <Route exact path='/auth' component={AuthForm}/>
         <Route exact path='/profile/:id' component={UserProfile}/>
+        <Route exact path='/profile/:id/add_pet' component={AddPet}/>
         <Route exact path='/pet/:id' component={PetProfile}/>
       </Switch>
     );
